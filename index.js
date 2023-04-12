56086c82688e0a277dee35d1646ed93719f494dd
Andrej <antandrej2003@gmail.com>
2023-04-12 15:48:33 +0200

    Adding get and post

---
+++ index.js
@@ -0,0 +1,44 @@
+const client = require('./connection.js')
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get('/api/users', (req, res) => {
+    client.query('SELECT * FROM usersp ORDER BY id', (err, result) =>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+});
+
+app.get('/api/sessions', (req, res) => {
+    client.query('SELECT * FROM sessions ORDER BY id', (err, result) =>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+});
+
+app.post('/api/sessions', (req, res) => {
+    const session = req.body;
+    let insertQuery = `INSERT INTO sessions(title, name, modname, picture) VALUES('${session.title}', '${session.name}', '${session.modname}', '${session.picture}')`;
+
+    client.query(insertQuery, (err, result) =>{
+        if(!err){
+            res.send('Session added!');
+        }
+        else{
+            res.send(err.message);
+        }
+    });
+    client.end;
+});
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Listening on port ${port} . . .`));
+
+client.connect();
